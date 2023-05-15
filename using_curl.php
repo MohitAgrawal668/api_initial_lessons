dd4d1cce91df8626283ab950083377bef78001d1
Mohit Agrawal <agrawalm668@gmail.com>
2023-05-15 18:21:25 +0530

    used stripe api to add customer on stripe dashboard

--- using_curl.php
+++ using_curl.php
@@ -1,19 +1,21 @@
 <?php
+    $api_key="sk_test_51N80laSAaB4KoL2VxtQwr42MBaOdrUg4WlilWzocqkYj9JN33eFOQuNgrzZczFgTfLDPsQ6KVXWCa7bqbtJK44Om00d8YzUKiI";
     $ch = curl_init();
 
-    $headers=[
-        "USER-Agent:MohitAgrawal668"
+    
+    $data=[
+        "name"=>"Mohit",
+        "email"=>"agrawalm6682@gmail.com"
     ];
-
     
     curl_setopt_array($ch,[
-        CURLOPT_URL => "https://api.github.com/gists/33313cbe4d46b5f928cd7b554521c279",
+        CURLOPT_URL => "https://api.stripe.com/v1/customers",
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => $headers
+        CURLOPT_USERPWD => $api_key,
+        CURLOPT_POSTFIELDS=>http_build_query($data)
     ]);
     $response = curl_exec($ch);
 
     curl_close($ch);
-    $data = json_decode($response, true);
-    print_r($data);
+    echo $response;
 ?>
