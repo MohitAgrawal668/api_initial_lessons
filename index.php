77023573902466df2aadba459a54194230098869
Mohit Agrawal <agrawalm668@gmail.com>
2023-05-15 11:38:22 +0530

    using api data in my web page to show the age of entered name

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
-    $response = file_get_contents("https://randomuser.me/api");
-    $data = json_decode($response, true);
-    echo $data['results'][0]['name']['first'];
+    if(isset($_POST['submit']))
+        {
+            $name=$_POST['name'];
+            $response = file_get_contents("https://api.agify.io/?name=$name");
+            $data = json_decode($response, true);
+            $age = $data['age'];
+        }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <?php
+            if(isset($_POST['submit'])) {
+                echo "Your age is $age years";
+            }
+        ?>
+        <form method="POST" action="index.php">
+            <label>Name</label>
+            <input type="text" name="name" class="form-control" required>
+            <input type="submit" name="submit" value="Guess Age">
+        </form>
+    </body>
+</html>
