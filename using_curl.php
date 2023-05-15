59d0bb74c7e7b6f93ebcf55e074535405f9b2d88
Mohit Agrawal <agrawalm668@gmail.com>
2023-05-15 13:22:00 +0530

    instead of getting invidual response header we use a call back function to get all response header in an array

--- using_curl.php
+++ using_curl.php
@@ -5,11 +5,19 @@
         "Authorization: Client-ID yl7PdgAl3J9f3_4y8PhWdsW41UteDkvt0Jd4Hpve9HY"
     ];
 
+    $response_headers=[];
+
+    $header_callback = function($ch, $header) use (&$response_headers)
+        {
+             $len = strlen($header);
+             $response_headers[]=$header;
+             return $len;   
+        };
     curl_setopt_array($ch,[
         CURLOPT_URL => "https://api.unsplash.com/photos/random",
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_HTTPHEADER => $headers,
-        CURLOPT_HEADER => true
+        CURLOPT_HEADERFUNCTION => $header_callback
     ]);
     //curl_setopt($ch, CURLOPT_URL, "https://randomuser.me/api");
     //curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
@@ -17,13 +25,9 @@
 
     $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     
-    $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-    $content_length = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-
+    
     curl_close($ch);
     echo $status_code."\n";
-    echo $content_type."\n";
-    echo $status_length."\n";
+    print_r($response_headers);    
     echo $response."\n";
 ?>
