0b1a34e527572b7e07b65b7f2fa8c59c9a965047
Mohit Agrawal <agrawalm668@gmail.com>
2023-05-15 11:58:35 +0530

    added a function curl_getinfo to get the http code

--- using_curl.php
+++ using_curl.php
@@ -8,6 +8,9 @@
     //curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
     $response = curl_exec($ch);
 
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+ 
     curl_close($ch);
-    echo $response."/n";
+    echo $status_code."\n";
+    echo $response."\n";
 ?>
